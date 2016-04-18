9b3952ceb8c6de1eafa82d10818a5c3dc6c83c3b
nurtantioq <nurtantio.q@gmail.com>
2016-04-18 12:54:03 +0800

    Add New Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,28 +5,28 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-heading">New Dashboard</div>
 
                     <div class="panel-body">
-                        You are logged in!
+                        <div class="row">
+                            @foreach($ads as $ad)
+                                <div class="col-xs-6 col-lg-4">
+                                    <h2>{{ $ad->name }}</h2>
+                                    <p>
+                                        {{ $ad->category->name }} <br>
+                                        {{ $ad->location }} <br>
+                                        {{ price($ad->price) }}
+                                    </p>
+                                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                                </div><!--/.col-xs-6.col-lg-4-->
+                            @endforeach
+                        </div>
+                        <div class="row">
+                            {{ $ads->links() }}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            @foreach($ads as $ad)
-                <div class="col-xs-6 col-lg-4">
-                    <h2>{{ $ad->name }}</h2>
-                    <p>
-                        {{ $ad->category->name }} <br>
-                        {{ $ad->location }} <br>
-                        {{ price($ad->price) }}
-                    </p>
-                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-                </div><!--/.col-xs-6.col-lg-4-->
-            @endforeach
-            {{ $ads->links() }}
-        </div><!--/row-->
     </div>
 @endsection
